ee06ab4b14287355df1e60e87ee6decfebcfff21
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-10-14 08:56:33 +0300

    +

--- arr.Advanced/05.js
+++ arr.Advanced/05.js
@@ -1,11 +1,16 @@
 function processOddNumbers(arr){
 let oddIndex = arr.filter((x, i) => i % 2 != 0)
 
-
     let doubled = oddIndex.map(x => x * 2);
-  
 
     let result = doubled.reverse()
     console.log(result.join(" "));
 }
 processOddNumbers([3, 0, 10, 4, 7, 3] )
+
+
+/*
+arr => arr.filter((x, i) => i % 2 != 0)
+.map(x => x * 2)
+.reverse()
+.join(" "); */
