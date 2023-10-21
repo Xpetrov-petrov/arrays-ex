c794ab848799ac92515ac06d6beeaf49908beea3
Xpetrov-petrov <p.petrov3333@abv.bg>
2023-10-21 15:51:07 +0300

    ++

---
+++ midEx/numbers.03.js
@@ -0,0 +1,14 @@
+function numbers(input) {
+    let arrOfNumns = input.split(' ').map(Number);
+    let reduced = arrOfNumns.reduce((acc, val) => acc + val) / arrOfNumns.length;
+    let topNums = arrOfNumns.filter(num => num > reduced).sort((a, b) => b - a).slice(0, 5);
+
+    if (topNums.length == 0) {
+        console.log('No');
+    } else {
+        console.log(topNums.join(' '));
+    }
+
+}
+//numbers('10 20 30 40 50');
+numbers('5 2 3 4 -10 30 40 50 20 50 60 60 51');
